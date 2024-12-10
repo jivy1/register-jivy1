68313a7127e65abb6b992d96d6804577039ec88b
jivy1 <74936073+jivy1@users.noreply.github.com>
2024-12-10 17:38:36 -0600

    Update games.php

--- games.php
+++ games.php
@@ -18,6 +18,28 @@ if (isset($_SESSION['username'])) {
         Display games here...
         <?php
         // put your code here
+        $sql = "SELECT * FROM games order by id";
+        $result = $conn->query($sql);
+
+        echo "<table border='1'>
+                <tr>
+                <th>ID</th>
+                <th>Opponent</th>
+                <th>Site</th>
+                <th>Result</th>
+                </tr>";
+
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row['id'] . "</td>";
+            echo "<td>" . $row['opponent'] . "</td>";
+            echo "<td>" . $row['site'] . "</td>";
+            echo "<td>" . $row['result'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+
+        $conn->close();
         ?>
     </body>
 </html>
