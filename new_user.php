676ccc260d39065e092ae040f589e139e75199c5
jivy1 <74936073+jivy1@users.noreply.github.com>
2024-12-10 17:44:28 -0600

    Update new_user.php

--- new_user.php
+++ new_user.php
@@ -1,17 +1,46 @@
 <?php
-// session start here...
 
-// get all 3 strings from the form (and scrub w/ validation function)
+session_start();
 
-// make sure that the two password values match!
+include_once 'validate.php';
+$endUser = test_input($_POST['user']);
+$endUserPassword = test_input($_POST['pwd']);
+$repeatPassword = test_input($_POST['repeat']);
+
+if ($endUserPassword != $repeatPassword) {
+    header("location: register.php");
+} else {
+    $hashedPassword = password_hash($endUserPassword, PASSWORD_DEFAULT);
+}
 
 
-// create the password_hash using the PASSWORD_DEFAULT argument
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "softball";
 
-// login to the database
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql = $conn->prepare("SELECT * FROM users WHERE username = '$endUser'");
+$sql->execute();
+$sql->store_result();
 
-// make sure that the new user is not already in the database
+if ($sql->num_rows > 0) {
+    // User already exists, redirect back to register.php
+    header("location: register.php");
+    exit();
+} else {
+    $sql = "INSERT INTO users (username, password) VALUES ('$endUser','$hashedPassword')";
+   
+}
 
-// insert username and password hash into db (put the username in the session
-// or make them login)
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
+$conn->close();
+?>
